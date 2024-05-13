3ce98ca0373078e7fb4ca76912dfa0d7d4c043e1
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-05-13 13:04:02 +0200

    ???

--- src/main/java/kea/exercise/Main.java
+++ src/main/java/kea/exercise/Main.java
@@ -4,16 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Person[] people = {
-            new Person("Alice", 25),
-            new Person("Bob", 20),
-            new Person("Charlie", 30)
-        };
-        System.out.println("Before sorting:");
-        System.out.println(Arrays.toString(people));
-        java.util.Arrays.sort(people);
-        System.out.println("After sorting:");
-        System.out.println(java.util.Arrays.toString(people));
+
     }
 }
